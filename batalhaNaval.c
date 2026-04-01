b58a793656216a8129812b4476202e5ac7b004c4
Max <121060984+MaximianoSantos@users.noreply.github.com>
2026-04-01 02:23:14 +0000

    Execução do nível novato, com criação do tabuleiro e posicionamneto dos navios.

--- batalhaNaval.c
+++ batalhaNaval.c
@@ -5,10 +5,76 @@
 // Siga os comentários para implementar cada parte do desafio.
 
 int main() {
-    // Nível Novato - Posicionamento dos Navios
-    // Sugestão: Declare uma matriz bidimensional para representar o tabuleiro (Ex: int tabuleiro[5][5];).
-    // Sugestão: Posicione dois navios no tabuleiro, um verticalmente e outro horizontalmente.
-    // Sugestão: Utilize `printf` para exibir as coordenadas de cada parte dos navios.
+    //Área para declaração de variaveis.
+    int index = 0;
+    
+    //Área para declaração das matrizes.
+    int tabuleiro [10][10] = {
+        {0, 0, 0, 0, 0, 0, 0, 0, 0, 0},
+        {0, 0, 0, 0, 0, 0, 0, 0, 0, 0},
+        {0, 0, 0, 0, 0, 0, 0, 0, 0, 0},
+        {0, 0, 0, 0, 0, 0, 0, 0, 0, 0},
+        {0, 0, 0, 0, 0, 0, 0, 0, 0, 0},
+        {0, 0, 0, 0, 0, 0, 0, 0, 0, 0},
+        {0, 0, 0, 0, 0, 0, 0, 0, 0, 0},
+        {0, 0, 0, 0, 0, 0, 0, 0, 0, 0},
+        {0, 0, 0, 0, 0, 0, 0, 0, 0, 0},
+        {0, 0, 0, 0, 0, 0, 0, 0, 0, 0}
+    };
+
+    //Área para declaração de vetores.
+    int navio1[3] = {3,3,3};
+    int navio2[3] = {3,3,3};
+
+    //Impressão do tabuleiro sem os návios.
+    printf("Tabuleiro de Batalha Naval\n");
+
+    printf("   A B C D E F G H I J\n");
+    for(int i = 0; i < 10; i++)
+    {
+        index++;
+        (index == 10) ? printf("%d ", index) : printf("%d  ", index);
+
+        for(int j = 0; j < 10; j++)
+    {
+            printf("%d ", tabuleiro[i][j]);         
+    }
+        printf("\n");   
+    }
+    //Posicionamento dos návios inseridos por vetores.
+   for(int i = 1, j = 0; i < 5, j < 3; i++, j++)
+   {
+        tabuleiro[i][7] += navio1[j];
+   }
+
+    for(int i = 4, j = 0; i < 5, j < 3; i++, j++)
+   {
+        tabuleiro[4][i] += navio1[j];
+   }
+
+   printf("\n");
+
+    //Impressão do tabuleiro com os návios.
+   printf("Tabuleiro de Batalha Naval Posicionado\n");
+    printf("   A B C D E F G H I J\n");
+
+    index = 0;
+
+   for(int i = 0; i < 10; i++)
+   {
+        index++;
+        (index == 10) ? printf("%d ", index) : printf("%d  ", index);
+        for(int j = 0; j < 10; j++)
+   {
+       printf("%d ", tabuleiro[i][j]);         
+   }
+     printf("\n");   
+   }
+
+
+
+
+
 
     // Nível Aventureiro - Expansão do Tabuleiro e Posicionamento Diagonal
     // Sugestão: Expanda o tabuleiro para uma matriz 10x10.
