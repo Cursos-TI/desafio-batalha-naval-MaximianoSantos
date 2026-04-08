36a72ffdd1682df58e9401c641e49e501f95b9a4
Max <121060984+MaximianoSantos@users.noreply.github.com>
2026-04-08 00:30:53 +0000

    Execução do nivel mestre, com implementação de matrizes de poderes especiais.

--- batalhaNaval.c
+++ batalhaNaval.c
@@ -1,15 +1,15 @@
 #include <stdio.h>
 
-// Desafio Batalha Naval - MateCheck
-// Este código inicial serve como base para o desenvolvimento do sistema de Batalha Naval.
-// Siga os comentários para implementar cada parte do desafio.
+#define linha 10
+#define coluna 10
 
 int main() {
     //Área para declaração de variaveis.
     int index = 0;
     
+    
     //Área para declaração das matrizes.
-    int tabuleiro [10][10] = {
+    int tabuleiro [linha][coluna] = {
         {0, 0, 0, 0, 0, 0, 0, 0, 0, 0},
         {0, 0, 0, 0, 0, 0, 0, 0, 0, 0},
         {0, 0, 0, 0, 0, 0, 0, 0, 0, 0},
@@ -23,32 +23,33 @@ int main() {
     };
 
     //Área para declaração de vetores.
-    int navio1[3] = {3,3,3};
-    int navio2[3] = {3,3,3};
-    int navio3[3] = {3,3,3};
-    int navio4[3] = {3,3,3};
+    //int navio1[3] = {3,3,3};
+    //int navio2[3] = {3,3,3};
+    //int navio3[3] = {3,3,3};
+    //int navio4[3] = {3,3,3};
+    
 
     //Impressão do tabuleiro sem os návios.
     printf("Tabuleiro de Batalha Naval\n");
 
     printf("   A B C D E F G H I J\n");
-    for(int i = 0; i < 10; i++)
+    for(int i = 0; i < linha; i++)
     {
         index++;
         (index == 10) ? printf("%d ", index) : printf("%d  ", index);
 
-        for(int j = 0; j < 10; j++)
+        for(int j = 0; j < coluna; j++)
     {
             printf("%d ", tabuleiro[i][j]);         
     }
         printf("\n");   
     }
-    //Posicionamento dos návios inseridos por vetores na horizontal
-    //e na vertical.
-   for(int i = 1, j = 0; i < 5, j < 3; i++, j++)
-   {
+    /*Posicionamento dos návios inseridos por vetores na horizontal
+    e na vertical.*/
+    /*for(int i = 1, j = 0; i < 5, j < 3; i++, j++)
+    {
        
-            tabuleiro[i][7] += navio1[j];     
+             tabuleiro[i][7] += navio1[j];     
         
    }
 
@@ -56,7 +57,7 @@ int main() {
    {
         tabuleiro[4][i] += navio2[j];
    }
-    //Posicionamento dos návios inseridos por vetores na diagonal.
+    Posicionamento dos návios inseridos por vetores na diagonal.
     
    for(int i = 0, j = 0; i < 5, j < 3; i++, j++)
    {
@@ -65,13 +66,13 @@ int main() {
 
     for(int i = 0, j = 0; i < 5, j < 3; i++, j++)
    {
-        tabuleiro[8-i][1+i] += navio4[j];
-   }
+       tabuleiro[8-i][1+i] += navio4[j];
+    }
 
    printf("\n");
 
-    //Impressão do tabuleiro com os návios.
-   printf("Tabuleiro de Batalha Naval Posicionado\n");
+    Impressão do tabuleiro com os návios.
+    printf("Tabuleiro de Batalha Naval Posicionado\n");
     printf("   A B C D E F G H I J\n");
 
     index = 0;
@@ -81,42 +82,74 @@ int main() {
         index++;
         (index == 10) ? printf("%d ", index) : printf("%d  ", index);
         for(int j = 0; j < 10; j++)
+/{
+       printf("%d ", tabuleiro[i][j]);         
+    }
+     printf("\n");   
+   }*/
+
+   //Área para implementação de poderes especiais.
+
+   //Cone.  
+   for(int i = 0; i < linha; i++ )
+   {
+        for(int j = 0; j < coluna; j++)
+        {                   
+          if(i == 0 && j==2 || i == 1 && j > 0 && j < 4 || i == 2 && j >= 0 && j <= 4)
+          {
+            tabuleiro[i][j] = 1;
+          }                                 
+        }
+    
+    printf("\n");    
+   }
+   //Cruz.    
+   for(int i = 0; i < linha; i++ )
+   {
+        for(int j = 0; j < coluna; j++)
+        {           
+          
+          if(i == 7 && j == 7 || i == 8 && j >= 5 && j <= 9 || i == 9 && j == 7)
+          {
+            tabuleiro[i][j] = 4;
+          }                    
+        }
+        printf("\n");
+   }
+   //Octaedro.   
+   for(int i = 0; i < linha; i++ )
+   {
+        for(int j = 0; j < coluna; j++)
+        {           
+          
+          if(i == 4 && j == 3 || i == 5 && j >= 2 && j <= 4 || i == 6 && j == 3)
+          {
+            tabuleiro[i][j] = 5;
+          }               
+        }
+        printf("\n");
+   }
+   printf("\n");
+   
+   
+   printf("Tabuleiro de Batalha Naval com poderes\n");
+    printf("   A B C D E F G H I J\n");
+
+    index = 0;
+
+   for(int i = 0; i < linha; i++)
+   {
+        index++;
+        (index == 10) ? printf("%d ", index) : printf("%d  ", index);
+        for(int j = 0; j < coluna; j++)
    {
        printf("%d ", tabuleiro[i][j]);         
    }
      printf("\n");   
    }
 
+   
 
-
-
-
-
-    // Nível Aventureiro - Expansão do Tabuleiro e Posicionamento Diagonal
-    // Sugestão: Expanda o tabuleiro para uma matriz 10x10.
-    // Sugestão: Posicione quatro navios no tabuleiro, incluindo dois na diagonal.
-    // Sugestão: Exiba o tabuleiro completo no console, mostrando 0 para posições vazias e 3 para posições ocupadas.
-
-    // Nível Mestre - Habilidades Especiais com Matrizes
-    // Sugestão: Crie matrizes para representar habilidades especiais como cone, cruz, e octaedro.
-    // Sugestão: Utilize estruturas de repetição aninhadas para preencher as áreas afetadas por essas habilidades no tabuleiro.
-    // Sugestão: Exiba o tabuleiro com as áreas afetadas, utilizando 0 para áreas não afetadas e 1 para áreas atingidas.
-
-    // Exemplos de exibição das habilidades:
-    // Exemplo para habilidade em cone:
-    // 0 0 1 0 0
-    // 0 1 1 1 0
-    // 1 1 1 1 1
-    
-    // Exemplo para habilidade em octaedro:
-    // 0 0 1 0 0
-    // 0 1 1 1 0
-    // 0 0 1 0 0
-
-    // Exemplo para habilidade em cruz:
-    // 0 0 1 0 0
-    // 1 1 1 1 1
-    // 0 0 1 0 0
-
+   
     return 0;
 }
